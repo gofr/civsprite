1596da4cd57a4fc235607429857635ec7a24be6e
gofr <32750931+gofr@users.noreply.github.com>
2020-04-29 20:48:14 +0200

    Progress slightly on implementation of .txt format
    And get rid of the distinction between the images and image index
    here too.

--- sprite/format/txt.py
+++ sprite/format/txt.py
@@ -4,59 +4,46 @@ The text-based format is a comma-separated values format similar to what Civ2
 itself uses.
 """
 
+import collections
+
 import sprite.objects
 
-
-SOURCES_HEADER = '@SOURCES'
-SOURCES_HELP = """\
-; Image sources
-;
-; A list of all images used by the static or animated sprites. They can be
-; referenced by number in later sections. The first image is number 0.
-;
-; Image file path, img x, img y, width, height, mask file path, mask x, mask y
-;
-;   image file path = Image file path relative to the directory this text file
-;                     is in.
-;   x, y = Top-left coordinate of the image or mask in the image file.
-;   width, height = dimensions of the part of the image to use. Not repeated
-;                   for the mask because that must be the same size.
-;   mask file path = If the image uses a civ-specific color, the path to a
-;                    black-and-white image that specifies which pixels of the
-;                    above-mentioned image should be turned into the civ shade.
-;
-; If you don't use a mask, you can leave out the mask file path and its x and y
-; coordinates. If the mask path is empty (whitespace only), it will use the
-; same file path as that of the image.
-;
-"""
 
 IMAGES_HEADER = '@IMAGES'
 IMAGES_HELP = """\
 ; Images
 ;
-; Name, image source number
+; A list of all images used by the static or animated sprites.
 ;
-;   Name = Arbitrary name meant primarily for humans to identify the image.
-;          Mainly useful to name the static sprites.
-;   Image source number = Index into the list of image sources. The first image
-;                         source in that list has index 0.
+; Name, image path, img x, img y, width, height, mask path, mask x, mask y
+;
+;   name = Unique name to identify the image.
+;   image path = Path to the image, relative to directory of this text file.
+;   x, y = Top-left coordinate of the image or mask in the image file.
+;   width, height = dimensions of the part of the image to use. Not repeated
+;                   for the mask because that must be the same size.
+;   mask path = If the image uses a civ-specific color, the path to a
+;               black-and-white image that specifies which pixels of the
+;               above-mentioned image should be turned into the civ shade.
+;
+; If you don't use a mask, you can leave out the mask path and its x and y
+; coordinates. If the mask path is empty (whitespace only), it will use the
+; same file path as that of the image.
 ;
 """
-# TODO: Isn't the whole @IMAGES section only useful for Static.spr files?
-# Should I use this section only for non-animated sprites? If the sprites are
-# animated, reference @SOURCES directly in @FRAMES?
-# Would this cause any problems exporting any of ToT's existing .spr files?
+Source = collections.namedtuple('Image', [
+    'name', 'image_path', 'image_x', 'image_y', 'width', 'height',
+    'mask_path', 'mask_x', 'mask_y'], defaults=(None, None, None))
 
 FRAMES_HEADER = '@FRAMES'
 FRAMES_HELP = """\
 ; Animation Frames
 ;
-; Image name, transparency, start, end_loop, mirror, end, continuous
+; Image, transparency, start, end_loop, mirror, end, continuous
 ;
-;   Image name = The image to use for this frame, using the name from the
-;                Images section.
-;                (You can re-use the same image in multiple frames.)
+;   Image = The image (defined above) to use for this frame. You can reference
+;           it either by its name or its index number (the first image is 0).
+;           You can re-use the same image in multiple frames.
 ;   transparency = A value 0 to 7, where 0 is fully opaque and 7 is 7/8
 ;                  transparent.
 ;   start = Is this frame the start of an animation? 0=no, 1=yes
@@ -71,6 +58,9 @@ FRAMES_HELP = """\
 ;                this set to 1.
 ;
 """
+Frame = collections.namedtuple('Frame', [
+    'image', 'transparency',
+    'start', 'end_loop', 'mirror', 'end', 'continuous'])
 
 ANIMATIONS_HEADER = '@ANIMATIONS'
 ANIMATIONS_HELP = """\
@@ -78,12 +68,13 @@ ANIMATIONS_HELP = """\
 ;
 ; Name, index of first frame
 ;
-;   Name = Arbitrary name meant for humans to identify the animation.
+;   Name = Unique name to identify the animation.
 ;   Index = Index into the list of frames that indicates with which frame this
 ;           animation starts. Counting starts at 0.
 ;           (You can use the same start frame multiple times if you want
 ;           multiple animations to look the same.)
 """
+Animation = collections.namedtuple('Animation', ['name', 'frame'])
 
 
 def load(path):
