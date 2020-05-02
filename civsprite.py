8f83de8e564b4c0134a56e27a2891daecbe72726
gofr <32750931+gofr@users.noreply.github.com>
2020-05-03 01:20:37 +0200

    Add some not-yet-supported flags for image output format options

--- civsprite.py
+++ civsprite.py
@@ -73,6 +73,44 @@ For example:
     parser.add_argument(
         'output', type=_output_format,
         help='path to the output file, e.g. a new .spr file to create')
+# NOTE: With the exception of Fantasy/Unit79.spr (Worm) and invisible end
+# frames, there are no .spr files that share images between multiple
+# animations, unless the entire animation is the same, like in mirrored
+# East/West animations.
+#
+# The Worm has buggy Attack animations, with a loop in the middle, so we never
+# see the intended end frames, and some of the Die animations continue one
+# frame too far, and then fade out what should've been the first frame of the
+# next animation. Because of this, the "Die S" and "Idle N" animations share
+# one frame.
+#
+# This means that, if I fix the broken Worm, all of ToT's sprites could be
+# exported to storyboard image files without there being any frames shared
+# between multiple animations.
+#
+# So while the sprite file format allows for this, perhaps I shouldn't try to
+# make other formats support or optimize this.
+#
+# All current .spr files are under 1.5MB anyway. People have hundreds of GBs
+# of disk space these days. They'll survive a few duplicate frames.
+#
+# Another NOTE: The majority of all existing animations have 12 or fewer unique
+# images per animation. Only a handful have more than 20. The longest unit
+# animation has 41. The longest resource animation has 20.
+#
+# So it should be reasonable to make the --as-storyboard flag save:
+# * each unit as a single row of N/NE/E/SE/S images for the Static.spr.
+# * each unit/resource animation as a single row of images.
+    parser.add_argument(
+        '--as-storyboard', action='store_true',
+        help='instead of storing each image as a separate file')
+    parser.add_argument(
+        '--adjacent-mask', action='store_true',
+        help='''instead of writing the mask to a separate image file, write it
+                to the same file, below the image it applies to''')
+    parser.add_argument(
+        '--with-border', action='store_true',
+        help='add a single pixel, green border around every image')
     args = parser.parse_args()
 
     convert(args.input, args.output)
