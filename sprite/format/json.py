cbe8babebecc0450ede96435bc76ef69cdaad28c
gofr <32750931+gofr@users.noreply.github.com>
2020-05-15 21:12:27 +0200

    Add TODO for making JSON format implementation

---
+++ sprite/format/json.py
@@ -0,0 +1,6 @@
+# TODO: Get rid of XML format and add JSON format.
+# I don't want to maintain the XML format anymore, get rid of it.
+# But add another format that:
+# * is easier for programmatic processing than the CSV-ish text format,
+# * tests my file format handling more by having more than one format and
+# * makes it easier to think about how to organize my objects.
